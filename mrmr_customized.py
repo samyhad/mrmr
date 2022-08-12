166e0ccea34efb29df56097831f965a04a53b4bc
Samira Haddad <samira.had.2000@gmail.com>
2022-08-12 03:49:52 -0300

    Customizando mrmr

--- mrmr_customized.py
+++ mrmr_customized.py
@@ -1,13 +1,8 @@
 from sklearn.base import BaseEstimator, TransformerMixin
 from mrmr import mrmr_classif
+import pandas as pd
 
 class mrmr_customized(BaseEstimator, TransformerMixin):
-    """
-    Do a bunch of fancy, specialized text normalization steps,
-    like tokenization, part-of-speech tagging, lemmatization
-    and stopwords removal.
-    """
-    ...
 
     def __init__(self, k):
         self.k = k
@@ -16,11 +11,20 @@ class mrmr_customized(BaseEstimator, TransformerMixin):
         #selected_features = mrmr_classif(X=X, y=y, K=self.k)
         #self.sf = selected_features
         # do the special stuff here
-        return mrmr_classif(X=X, y=y, K=self.k)
+        #return mrmr_classif(X=X, y=y, K=self.k)
+
+        df_y = pd.DataFrame(y)
+        df_X = pd.DataFrame(X)
+        sc = mrmr_classif(X=df_X, y=df_y, K=self.k)
+        df_selected_features = df_X[sc]
+        return df_selected_features
 
     def fit(self, X, y=None):
-        return mrmr_classif(X=X, y=y, K=self.k)
+        df_y = pd.DataFrame(y)
+        df_X = pd.DataFrame(X)
+        sc = mrmr_classif(X=df_X, y=df_y, K=self.k)
+        df_selected_features = df_X[sc]
+        return df_selected_features
 
-    """def transform(self, documents):
-        for document in documents:
-            yield self.normalize(document)"""
+    def transform(self, X):
+        pass
--- test.py
+++ test.py
@@ -24,7 +24,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
 import mrmr_customized
 #from . import mrmr_customized
 
-mrmr_filter = mrmr_customized(k=10)
+mrmr_filter = mrmr_customized.mrmr_customized(k=10)
 clf = LinearSVC()
 mrmr_pipe = make_pipeline(mrmr_filter, clf)
 mrmr_pipe.fit(X_train, y_train)
