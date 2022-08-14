d6f1833790fc0fa82399ca041bcab7a86078221f
Samira Haddad <samira.had.2000@gmail.com>
2022-08-14 10:13:51 -0300

    Customizando mrmr

--- mrmr_customized_old.py
+++ mrmr_customized_old.py
@@ -6,16 +6,25 @@ class mrmr_customized(BaseEstimator, TransformerMixin):
 
     def __init__(self, k):
         self.k = k
-        self.df_selected_features = []
 
-    def mrmr(self, X, y):
+    def mrmr_(self, X, y):
+        #selected_features = mrmr_classif(X=X, y=y, K=self.k)
+        #self.sf = selected_features
+        # do the special stuff here
+        #return mrmr_classif(X=X, y=y, K=self.k)
+
         df_y = pd.DataFrame(y)
         df_X = pd.DataFrame(X)
         sc = mrmr_classif(X=df_X, y=df_y, K=self.k)
-        self.df_selected_features = df_X[sc]
+        df_selected_features = df_X[sc]
+        return df_selected_features
 
     def fit(self, X, y=None):
-        return self
+        df_y = pd.DataFrame(y)
+        df_X = pd.DataFrame(X)
+        sc = mrmr_classif(X=df_X, y=df_y, K=self.k)
+        df_selected_features = df_X[sc]
+        return df_selected_features
 
-    def transform(self, X, y):
-        return self.mrmr(X, y)
+    def transform(self, X):
+        pass
